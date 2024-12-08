7394e62050d558230980024a95195ec851e7265b
Jesper F <jesperfagerberg@live.se>
2024-12-08 23:16:58 +0100

    Day 8 complete

--- DaysCode/Day8.cs
+++ DaysCode/Day8.cs
@@ -84,15 +84,96 @@ namespace AdventOfCode2024.Days
             // Write result
             Console.WriteLine("Total antinodes:  " + allAntiNodeSets.Select(x=>x.Count).Sum());
             Console.WriteLine("Unique antinodes: " + uniqueAntinodes.Count);
-
-
-            //Lookup<Node, Node> lookupList = new Lookup<Node, Node>();
-
         }
 
         public void RunSecondStar(DayDataType fullOrSimpleData)
         {
+            List<Node> empties = new List<Node>();
+            Dictionary<char, List<Node>> antennaNodes = new Dictionary<char, List<Node>>();
 
+            string[] data = DataLoader.LoadRowData(8, fullOrSimpleData);
+            char[,] grid = new char[data[0].Length, data.Length];
+            int startCursorPosFromTop = Console.CursorTop;
+            for (int y = 0; y < data.Length; y++)
+            {
+                for (int x = 0; x < data[0].Length; x++)
+                {
+                    char c = data[y][x];
+                    grid[x, y] = c;
+
+                    if (c == '.')
+                    {
+                        Node newNode = new Node(new Int2(x, y), c);
+                        //if (empties)
+                        empties.Add(newNode);
+                        TextUtilities.CFW("@DGy" + c);
+                    }
+                    else
+                    {
+                        Node newNode = new Node(new Int2(x, y), c);
+                        if (antennaNodes.TryGetValue(c, out var nodes))
+                        {
+                            nodes?.Add(newNode);
+                        }
+                        else antennaNodes.Add(c, [newNode]);
+                        TextUtilities.CFW("@Whi" + c);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            DataMap<char> charMap = new DataMap<char>(grid);
+            HashSet<Int2> uniqueAntinodes = new HashSet<Int2>();
+            List<List<Int2>> allAntiNodeSets = new List<List<Int2>>();
+
+            // For each set of nodes with the same character...
+            foreach (var nodeSet in antennaNodes)
+            {
+                // ...we loop through all the nodes...
+                foreach (var thisNode in nodeSet.Value)
+                {
+                    List<Int2> antinodes = new List<Int2>();
+                    // ...to compare with eachother (except itself).
+                    foreach (var otherNode in nodeSet.Value)
+                    {
+                        // Skip self
+                        if (thisNode == otherNode) continue;
+
+                        // For part two, we only need to modify what will be the new antinodes
+                        // Essentially, instead of skipping over the antenna, we place an
+                        // antinode at the antenna, and also at all positions beyond it the same 
+                        // distance away. See the puzzle
+                        // Calculate the position of the antinode
+                        Int2 diff = otherNode.Position - thisNode.Position;
+                        int counter = 1;
+                        Int2 newAntinodePos = Int2.Zero;
+                        do
+                        {
+                            newAntinodePos = thisNode.Position + counter * diff;
+
+                            // Add them to the antinodes
+                            antinodes.Add(newAntinodePos);
+                            uniqueAntinodes.Add(newAntinodePos);
+
+                            counter++;
+
+                            // Write the antinode
+                            Console.SetCursorPosition(newAntinodePos.x, startCursorPosFromTop + newAntinodePos.y);
+                            TextUtilities.CFW("@Gre" + thisNode.Character);
+                        }
+                        while (charMap.IsInside(newAntinodePos + diff));
+
+                    }
+                    allAntiNodeSets.Add(antinodes);
+                }
+            }
+
+            // Move cursor down to below the map
+            Console.SetCursorPosition(0, startCursorPosFromTop + charMap.Dim.y + 1);
+
+            // Write result
+            Console.WriteLine("Total antinodes:  " + allAntiNodeSets.Select(x => x.Count).Sum());
+            Console.WriteLine("Unique antinodes: " + uniqueAntinodes.Count);
         }
 
         record Node(Int2 Position, char Character);
