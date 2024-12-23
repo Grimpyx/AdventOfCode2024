075c7d70c8280d5c29a681bd04f1c4693f0e42b0
Jesper F <jesperfagerberg@live.se>
2024-12-23 12:41:48 +0100

    Day 17 cleaned up

--- DaysCode/Day17.cs
+++ DaysCode/Day17.cs
@@ -13,7 +13,9 @@ namespace AdventOfCode2024.Days
             ((long A, long B, long C) reg, int[] opcodes) = ParseProgram(DataLoader.LoadRowData(17, fullOrSimpleData));
             register = reg;
 
-            while(instructionPointer < opcodes.Length - 1)
+            outputs.Clear();
+            instructionPointer = 0;
+            while (instructionPointer < opcodes.Length - 1)
             {
                 int i = instructionPointer;
 
@@ -22,6 +24,8 @@ namespace AdventOfCode2024.Days
 
                 RunInstruction(program, operand);
             }
+
+            TextUtilities.CFWLine($"@GraOutput: @Gra[@Yel{string.Join("@DGy, @Yel", outputs)}@Gra]");
         }
 
         public void RunSecondStar(DayDataType fullOrSimpleData)
@@ -58,22 +62,19 @@ namespace AdventOfCode2024.Days
             // on and on and on.
             // To shape our A, we will walk backward and start at its last digit:
             // We first change by 8^15 to get the last digit (^1)
-            //  Then we change by 8^14 to get the last digit (^2)
-            //  Then we change by 8^13 to get the last digit (^3)
-
-            // Too high 110229415094682
-            // Too low 105637558184346
+            //  Then we change by 8^14 to get the second last digit (^2)
+            //  Then we change by 8^13 to get the third last digit (^3)
 
             // Thanks to Gabba for this solution.
             // I was trying to do this but something went wrong on the way
             // https://www.reddit.com/r/adventofcode/comments/1hg38ah/comment/m2l7qx8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
             // https://github.com/TrueNorthIT/AdventOfCode/blob/christian-waters/2024/day17/smart/Program.cs
 
-            ((long A, long B, long C) reg, int[] opcodes) parsedInput = ParseProgram(DataLoader.LoadRowData(17, fullOrSimpleData));
+            ((long A, long B, long C) reg, int[] opcodes) parsedInput = ParseProgram(DataLoader.LoadRowData(17, DayDataType.Full));
             List<long> goal = parsedInput.opcodes.Select(x => (long)x).ToList();
 
             long currentA = 0;
-            for (int goalIndex = goal.Count - 1; goalIndex >= 0; goalIndex -= 1)
+            for (int goalIndex = goal.Count - 1; goalIndex >= 0; goalIndex--)
             {
                 for (int i = 0; i < int.MaxValue; i++)
                 {
@@ -97,302 +98,10 @@ namespace AdventOfCode2024.Days
                 }
             }
 
+            // Print output
             TextUtilities.CFWLine($"@Gra >>> @Gre{currentA}@Gra / @Red{Convert.ToString(currentA, 8)} @Gra(@Gredecimal @Gra/ @Redoctal@Gra)");
 
-
-
-            return;
-
-            byte[] goals = parsedInput.opcodes.Select(x => (byte)x).ToArray();
-            int[] goalsMultiplier = new int[goals.Length];
-
-            currentA = -1;
-
-            List<byte> firstOutputs = new List<byte>();
-            int[] collected = new int[goals.Length];
-            for (int i = 0; i < collected.Length; i++)
-            {
-                collected[i] = 0;
-            }
-
-            for (int i = 0; i < 50; i++)
-            {
-                currentA += 1;
-                ExecuteProgram(currentA);
-                firstOutputs.Add(outputs[0]);
-
-                TextUtilities.CFWLine("@GraA: @Gre" + currentA.ToString("000") + " @Gre-> @Gre" + outputs[0]);
-                TextUtilities.CFWLine("@Yel" + string.Join("@DGy,@Yel", outputs) + "\n");
-            }
-            Console.WriteLine("\n\n");
-            currentA = 0;
-            for (int goalIndex = goals.Length - 1; goalIndex >= 0; goalIndex--)
-            {
-                for (int i = 0; i < 800; i++)
-                {
-
-                    byte goalByte = goals[goalIndex];
-                    if (goalByte == firstOutputs[i])
-                    {
-                        collected[goalIndex] += i;
-                        //currentA += collected[goalIndex] * (1L << (goalIndex * 3));
-                        break;
-                    }
-
-                    if (i == firstOutputs.Count - 1)
-                        throw new Exception("bombba");
-                }
-                //TextUtilities.CFWLine("@GraCollected: @Yel" + string.Join("@DGy,@Yel", collected));
-
-                //ExecuteProgram(currentA);
-                //TextUtilities.CFWLine("@GraOutput:    @Gre" + string.Join("@DGy,@Gre", outputs));
-                // Borrow digits
-                // 14/8 => 1, runs once with i = 0
-                for (int i = 0; i < (collected[goalIndex] / 8); i++)
-                {
-                    TextUtilities.CFWLine("@RedCollected:   @Red" + string.Join("@DGy,@Red", collected));
-                    Console.WriteLine(new string(' ', 2 * (goalIndex - 1) + 17) + 'V');
-                    TextUtilities.CFWLine("@GraGoal:          @Gre" + string.Join("@DGy,@Gre", goals));
-                    ExecuteProgram(GetA());
-                    TextUtilities.CFWLine("@MgnOutput Before: " + string.Join("@DGy,@Mgn", outputs));
-                    collected[goalIndex + 1] -= 2;
-                    ExecuteProgram(GetA());
-                    TextUtilities.CFWLine("@MgnOutput After:  " + string.Join("@DGy,@Mgn", outputs));
-                    //currentA -= (1L << ((goalIndex + 1) * 3));
-                    //TextUtilities.CFWLine("@RedCollected: @Red" + string.Join("@DGy,@Red", collected));
-                }
-                //ExecuteProgram(currentA);
-                //TextUtilities.CFWLine("@GraOutput:    @Gre" + string.Join("@DGy,@Gre", outputs));
-            }
-
-            // Assemble A
-            /*for (int c = collected.Length - 1; c >= 0; c--)
-            {
-                currentA += collected[c] * (1L << (c * 3));
-                ExecuteProgram(currentA);
-                Console.WriteLine(new string(' ', 2 * (c - 1) + 13) + 'V');
-                TextUtilities.CFWLine("@GraOutput:    @Red" + string.Join("@DGy,@Red", outputs));
-            }
-            ExecuteProgram(currentA);*/
-
-            currentA = GetA();
-
-            Console.WriteLine();
-            TextUtilities.CFWLine("@GraCollected: @Yel" + string.Join("@DGy,@Yel", collected));
-            TextUtilities.CFWLine("@GraA:         @Yel" + currentA);
-            TextUtilities.CFWLine("@GraGoal:      @Gre" + string.Join("@DGy,@Gre", goals));
-            TextUtilities.CFWLine("@GraOutput:    @Gre" + string.Join("@DGy,@Gre", outputs));
-            return;
-            Console.WriteLine();
-
-            long GetA()
-            {
-                long a = 0;
-                for (int c = collected.Length - 1; c >= 0; c--)
-                {
-                    a += collected[c] * (1L << (c * 3));
-                    //ExecuteProgram(a);
-                    //Console.WriteLine(new string(' ', 2 * (c - 1) + 13) + 'V');
-                    //TextUtilities.CFWLine("@GraOutput:    @Red" + string.Join("@DGy,@Red", outputs));
-                }
-                return a;
-            }
-
-            /*
-            // print values
-            for (int i = 0; i < 50; i++)
-            {
-                Console.Write(i + ": ");
-                TryGetDigit(i, 2, out byte b1);
-                Console.WriteLine();
-            }*/
-
-            /*TryGetDigit(341, 0, out byte b1);
-            Console.WriteLine();
-            TryGetDigit(341, 1, out byte b2);
-            Console.WriteLine();
-            TryGetDigit(341, 2, out byte b3);*/
-
-
-            // Loop through the digits backward
-            long stepSize1 = -1;
-            byte d1 = 127, d2 = 88, digitsAfter = 255;
-
-            // First iteration is special
-            do
-            {
-                currentA += 1L << ((goals.Length - 1) * 3);
-                ExecuteProgram(currentA);
-                d1 = GetDigit(goals.Length - 1);
-                TextUtilities.CFWLine("@GraA: @Gre" + currentA);
-                TextUtilities.CFWLine("@Yel" + string.Join("@DGy,@Yel", outputs) + "  @DRe" + goals[goals.Length - 1]);
-            } while (d1 != goals[goals.Length - 1]);
-
-
-            for (int goalIndex = goals.Length - 2; goalIndex >= 0; goalIndex--)
-            {
-                TextUtilities.CFW("@DGy" + new string(' ', goalIndex * 2));
-                TextUtilities.CFWLine("@RedV");
-
-                // Loop forward to find the first number
-                int timesAdded = 0;
-                d1 = GetDigit(goalIndex);
-                d2 = GetDigit(goalIndex + 1);
-                while (d1 != goals[goalIndex])
-                {
-                    currentA += 1L << (goalIndex * 3);
-                    timesAdded++;
-
-                    ExecuteProgram(currentA);
-                    d1 = GetDigit(goalIndex);
-                    d2 = GetDigit(goalIndex + 1);
-
-                    if (timesAdded > 7) // we have overflowed, meaning the correct answer is behind
-                    {
-                        // Reset to before
-                        currentA -= 1L << ((goalIndex + 1) * 3);
-                        timesAdded = 0;
-
-                        /*// Start looping backward
-                        while (d1 != goals[goalIndex] || d2 != goals[goalIndex + 1])
-                        {
-                            currentA -= 1L << (goalIndex * 3);
-                            timesAdded++;
-
-                            ExecuteProgram(currentA);
-                            d1 = GetDigit(goalIndex);
-                            d2 = GetDigit(goalIndex + 1);
-
-                            TextUtilities.CFWLine("@DYe" + string.Join("@DGy,@DYe", outputs) + "  @DRe" + goals[goalIndex]);
-
-                            if (timesAdded > 7) Console.WriteLine("ERROR");
-                        }*/
-                        ExecuteProgram(currentA);
-                        TextUtilities.CFW("@DGy" + new string(' ', goalIndex * 2));
-                        TextUtilities.CFWLine("@RedV");
-                        continue;
-                    }
-
-                    //TextUtilities.CFWLine("@Yel" + string.Join("@DGy,@Yel", outputs) + "  @DRe" + goals[goalIndex]);
-
-                    // If the digit after changes, we have overflowed
-                    /*if (d2 != goals[goalIndex + 1])
-                    {
-                        currentA -= 1L << ((goalIndex + 1) * 3); // Go back ONCE in the digit before this one
-                        ExecuteProgram(currentA);
-                        TextUtilities.CFWLine("@DYe" + string.Join("@DGy,@DYe", outputs) + "  @DRe" + goals[goalIndex]);
-                        break;
-                    }
-                    else*/
-                    TextUtilities.CFWLine("@Yel" + string.Join("@DGy,@Yel", outputs) + "  @DRe" + goals[goalIndex]);
-                }
-
-                // If any digit is wrong, we have digit overflow.
-                // How much should we go back?
-                /*for (int i = goalIndex + 1; i < goals.Length - 1; i++)
-                {
-                    digitsAfter = GetDigit(i);
-                    if (digitsAfter == goals[i])
-                        break; // If it is correct
-                    else
-                    {
-                        currentA -= 1L << (i * 3);
-                        currentA += 1L << ((i-1) * 3);
-                        ExecuteProgram(currentA);
-                        TextUtilities.CFWLine("@DYe" + string.Join("@DGy,@DYe", outputs) + "  @DRe" + goals[goalIndex]);
-                    }
-                }*/
-                /*digitsAfter = GetDigit(goalIndex + 1);
-                while (digitsAfter != goals[goalIndex+1])
-                {
-                    currentA -= 1L << ((goalIndex + 1) * 3);
-                    ExecuteProgram(currentA);
-                    TextUtilities.CFWLine("@DYe" + string.Join("@DGy,@DYe", outputs) + "  @DRe" + goals[goalIndex]);
-                    digitsAfter = GetDigit(goalIndex + 1);
-                }*/
-
-                TextUtilities.CFWLine("@Gre" + string.Join("@DGy,@Gre", outputs) + "  @Red" + goals[goalIndex] + "\n");
-            }
-
-            //TryGetDigit(currentA, goals.Length - 1, out _);
-            Console.WriteLine();
-            TextUtilities.CFWLine("@Gra >>> Goal:       @Yel" + string.Join("@DGy,@Yel", goals));
-            TextUtilities.CFWLine("@Gra >>> Matched to: @Yel" + string.Join("@DGy,@Yel", outputs));
-            TextUtilities.CFWLine("@Gra >>> Value of A: @Yel" + currentA);
-
-            //register = reg;
-
-            //List<List<long>> series = new List<List<long>>() { { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] }, { [] } };
-
-            //bool foundProgram = false;
-            //long startRegA = 0;
-
-            //long v = (1L << 48);
-
-            /*for (long counter = 0; counter < v; counter++)
-            {
-                outputs.Clear();
-                firstOut = true;
-
-                startRegA += 1;
-                register.A = startRegA;
-                instructionPointer = 0;
-
-                // Perform program
-                while (instructionPointer < opcodes.Length - 1)
-                {
-                    int i = instructionPointer;
-
-                    int program = opcodes[i];
-                    int operand = opcodes[i + 1];
-
-                    RunInstruction(program, operand);
-                }
-
-                for (int p = 0; p < outputs.Count; p++)
-                {
-                    series[p].Add(outputs[p]);
-                }
-
-                //TextUtilities.CFWLine($"@Red{startRegA.ToString("0000000000")}@Gra: @Yel" + string.Join("@DGy,@Yel", outputs));
-                if (startRegA <= (1 << 15)) TextUtilities.CFWLine(startRegA + ","+ outputs[0].ToString());
-            }*/
-
-            // Find loop
-            /*bool foundLoop = false;
-            int loopIndex = -1;
-            List<long> comparison = new List<long>(series[0][..500000]);
-            for (int i = 1; i < series[0].Count; i++)
-            {
-
-                for (int j = 0; j < comparison.Count && i+j < series.Count; j++)
-                {
-                    foundLoop = true;
-                    // If not the same, the is no loop
-                    
-                    if (comparison[j] != series[0][i+j])
-                    {
-                        foundLoop = false;
-                        break;
-                    }
-                }
-
-                if (foundLoop)
-                {
-                    loopIndex = i;
-                    break;
-                }
-                if (i % 1000 == 0) Console.WriteLine(i);
-            }*/
-
-            //TextUtilities.CFWLine("@Gra >>> Lowest value for register A is @Yel" + startRegA.ToString());
-
-            byte GetDigit(int digit)
-            {
-                if (outputs.Count - 1 < digit)
-                    return 255;
-                return outputs[digit];
-            }
+            // Methods below
 
             void ExecuteProgram(long A)
             {
